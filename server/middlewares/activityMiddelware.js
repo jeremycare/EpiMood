ac39defce0311789e175292f98d2d519ffe2211f
care_j <jeremy.care@epitech.eu>
2019-01-22 17:42:18 +0100

    feat(middleware): activity middleware to send only activities that concern the user who submit the query

---
+++ server/middlewares/activityMiddelware.js
@@ -0,0 +1,38 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/userModel')
+const Activity = require('../models/activityModel')
+
+const filterAdminActivity = () => {
+	return async (req, res, next) => {
+		console.log(req.user)
+		if (req.user.admin) {
+			Activity.find(function(err, activities) {
+				if (err) {
+					res.status(500).json(err)
+					return
+				}
+				req.activities = activities
+				next()
+			})
+		} else {
+			const activities = await Activity.find(
+				{
+					'users.user': req.user._id,
+				},
+				'name feedbacks.grade feedbacks.comment feedbacks.created_at date users'
+			).lean()
+			activities.forEach(x => {
+				x.voted = x.users.find(
+					vote => vote.user.toString() === req.user._id.toString()
+				).voted
+				delete x.users
+			})
+			req.activities = activities
+			next()
+		}
+	}
+}
+
+module.exports = {
+	filterAdminActivity: filterAdminActivity,
+}
